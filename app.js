0136f7b6c09fc13d6b74c7238f250236827e17fb
kennedy Costa <kennedy.fly454@gmail.com>
2022-10-26 10:35:02 -0400

    commit mini project

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const result = document.querySelector(".result");
+
+let check;
+
+function checkPalindrome() {
+  const inputSplited = input.value
+    .split("")
+    .filter((item) => {
+      return item != " ";
+    })
+    .join("")
+    .toLowerCase();
+
+  if (check === inputSplited) {
+    result.innerHTML = `<p>A palavra <span>${inputSplited}</span> é um palíndromo</p>`;
+  } else {
+    result.innerHTML = `<p>A palavra <span>${inputSplited}</span> não é palíndromo</p>`;
+  }
+
+  setTimeout(() => {
+    result.firstElementChild.remove();
+    input.focus()
+  }, 2000);
+
+  input.value = ''
+}
+
+input.addEventListener("input", (event) => {
+  event.preventDefault();
+  const inputValue = event.target.value;
+  check = inputValue
+    .split("")
+    .reverse()
+    .filter((item) => {
+      return item != " ";
+    })
+    .join("")
+    .toLowerCase();
+
+  if (inputValue) {
+    btn.classList.add("active");
+  } else {
+    btn.classList.remove("active");
+  }
+});
+
+btn.addEventListener("click", checkPalindrome);
